01b280b127a60c84a1608d808fba585fcc5cea23
mivnci <mivinci@qq.com>
2024-12-25 23:02:43 +0800

    rename stuffs and events

--- apps/web/src-tauri/src/main.rs
+++ apps/web/src-tauri/src/main.rs
@@ -1,6 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Manager, WindowEvent};
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
+
 fn main() {
-  ovo_app_lib::run()
+  tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_webview_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("'apply_vibrancy' is only supported on macOS");
+
+      Ok(())
+    })
+    .on_window_event(|window, event| match event {
+      WindowEvent::CloseRequested { api, .. } => {
+        window.hide().unwrap();
+        api.prevent_close();
+      }
+      _ => {}
+    })
+    .plugin(tauri_plugin_opener::init())
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
